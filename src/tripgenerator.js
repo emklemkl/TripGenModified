fb89225397f5c98c223568ee4b015afc8a646b9f
Julia Lind <juli22@student.bth.se>
2023-11-25 23:37:19 +0100

    first routes for gbg

--- src/tripgenerator.js
+++ src/tripgenerator.js
@@ -10,7 +10,7 @@ const geoTools = require('geo-tools');
 
 const tripGenerator = {
     city: "",
-    cityid: 2,
+    cityid: 3,
     /**
      * Coordinates of the city as polygon
      * Note! Loaded from file!
