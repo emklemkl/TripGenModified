fa5f2dcfec9e92afa24308ed668cae713389041c
Julia Lind <juli22@student.bth.se>
2023-11-27 19:58:05 +0100

    some reorganising in directory structure

---
+++ cities/json2csv.js
@@ -0,0 +1,38 @@
+/**
+ * This is a script to convert your json city files
+ * into data for csv that easily can be loaded into an SQL database
+ */
+
+const fs = require('fs');
+const zoneId = {
+    park: 1,
+    charge: 2,
+    forbidden: 3,
+}
+const speedLimit = 20;
+
+
+fs.writeFileSync("./city.csv", `'id','name', 'speed_limit', 'geometry'\r\n`);
+fs.writeFileSync("./zone_loc.csv", `'zone_id','city_id', 'date_from', 'geometry'\r\n`);
+
+for (const cityid of [1, 2, 3]) {
+    const city = require(`./${cityid}.json`);
+    fs.appendFileSync("./city.csv", `'${city.id}','${city.name}','${speedLimit}','${JSON.stringify(city.coords.geometry)}'\r\n`);
+    for (const zone of city.forbidden) {
+        if (zone.properties.role === "forbidden") {
+            fs.appendFileSync("./zone_loc.csv", `'${zoneId.forbidden}','${city.id}','2023-10-01','${JSON.stringify(zone.geometry)}'\r\n`);
+        }
+    }
+    for (const zone of city.park_zones) {
+        fs.appendFileSync("./zone_loc.csv", `'${zoneId.park}','${city.id}','2023-10-01','${JSON.stringify(zone.geometry)}'\r\n`);
+    }
+    for (const zone of city.charge_zones) {
+        fs.appendFileSync("./zone_loc.csv", `'${zoneId.charge}','${city.id}','2023-10-01','${JSON.stringify(zone.geometry)}'\r\n`);
+    }
+}
+
+
+
+
+
+
